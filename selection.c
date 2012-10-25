41758a2392b0064326ffb08ba07e19b945766d53
soumya mandi <soumyamandi@gmail.com>
2012-10-25 23:54:46 +0530

    code to read input from standard input with terminated by -1 added

---
+++ selection.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main( int argc,char** argv ) {
+
+	int* array = ( int* )malloc( sizeof( int ) * 1000 ) ;
+	int i ;
+	int in ;
+	while( scanf( &in ) ) {
+		printf("in is : %d\n",in ) ;
+	}
+
+
+
+}
